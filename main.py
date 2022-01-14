60b427549dac92aaa752c6d3449a869dd87989e0
Juan <juan.romero.eng@gmail.com>
2022-01-14 11:02:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,27 +22,43 @@ def calculate_frequency_magnitude_response(f, f_c, f_delta, f_delta_stop_perc, a
 
 def showPlots(f, f_c, f_c_1, f_c_2, abs_h_f):
 
-    plt.figure()
-    plt.plot(f, abs_h_f)
-    plt.xlim([0, 0.5])
-    plt.axvline(f_c_1, color='r')
-    plt.axvline(f_c, color='k')
-    plt.axvline(f_c_2, color='r')
-    plt.grid()
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel(r'$|h_(f)|$')
-    plt.title('Target Frequency Magnitude Response')
+    abs_h_f_dB = 10*np.log10(abs_h_f)
 
-    plt.figure()
-    plt.plot(f, 10*np.log10(abs_h_f))
-    plt.xlim([0, 0.5])
-    plt.axvline(f_c_1, color='r')
-    plt.axvline(f_c, color='k')
-    plt.axvline(f_c_2, color='r')
-    plt.grid()
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel(r'$|h_(f)|_{dB}$')
-    plt.title('Target Frequency Magnitude Decibel Response')
+    abs_h_f_idxs    = np.array([0, 0])
+    abs_h_f_dB_idxs = np.array([0, 1])
+    
+    idxs_matrix = np.vstack((abs_h_f_idxs, abs_h_f_dB_idxs))
+    num_rows = np.amax(idxs_matrix[:, 0]) + 1
+    num_cols = np.amax(idxs_matrix[:, 1]) + 1
+    [fig, axs] = plt.subplots(num_rows, num_cols, figsize=(10, 4))
+    if(len(axs.shape) == 2):
+        abs_h_f_axis    = axs[   abs_h_f_idxs[0],    abs_h_f_idxs[1]]
+        abs_h_f_dB_axis = axs[abs_h_f_dB_idxs[0], abs_h_f_dB_idxs[1]]
+    else:
+        abs_h_f_axis    = axs[np.amax(   abs_h_f_idxs)]
+        abs_h_f_dB_axis = axs[np.amax(abs_h_f_dB_idxs)]
+
+    abs_h_f_axis.plot(f, abs_h_f)
+    abs_h_f_axis.set_xlim([0, 0.5])
+    abs_h_f_axis.axvline(f_c_1, color='r')
+    abs_h_f_axis.axvline(f_c, color='k')
+    abs_h_f_axis.axvline(f_c_2, color='r')
+    abs_h_f_axis.grid()
+    abs_h_f_axis.set_xlabel('Frequency (Hz)')
+    abs_h_f_axis.set_ylabel(r'$|h_(f)|$')
+    abs_h_f_axis.set_title('Target Frequency Magnitude Response')
+
+    abs_h_f_dB_axis.plot(f, abs_h_f_dB)
+    abs_h_f_dB_axis.set_xlim([0, 0.5])
+    abs_h_f_dB_axis.axvline(f_c_1, color='r')
+    abs_h_f_dB_axis.axvline(f_c, color='k')
+    abs_h_f_dB_axis.axvline(f_c_2, color='r')
+    abs_h_f_dB_axis.grid()
+    abs_h_f_dB_axis.set_xlabel('Frequency (Hz)')
+    abs_h_f_dB_axis.set_ylabel(r'$|h_(f)|_{dB}$')
+    abs_h_f_dB_axis.set_title('Target Frequency Magnitude Decibel Response')
+
+    fig.tight_layout()
 
     plt.show()
 
