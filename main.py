801907fa9406d47854f89d1f487c78ec0375ad82
romero-eng <39012010+romero-eng@users.noreply.github.com>
2022-01-16 15:06:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal as dsp
 import matplotlib.pyplot as plt
 import utility
 
@@ -23,25 +24,33 @@ def calculate_frequency_magnitude_response(f_c, delta_f, transition_bandwidth, t
     return [f_c_1, f_c_2, f_c, f, abs_h_f, abs_h_f_dB]
 
 
-def showPlots(f_c, f_c_1, f_c_2, f, abs_h_f, abs_h_f_dB):
+def showPlots(f_c, f_c_1, f_c_2, f, abs_h_f, abs_h_f_dB, abs_h_f_emp, abs_h_f_emp_dB):
 
-    abs_h_f_idxs    = np.array([0, 0])
-    abs_h_f_dB_idxs = np.array([0, 1])
+    abs_h_f_idxs        = np.array([0, 0])
+    abs_h_f_dB_idxs     = np.array([0, 1])
+    abs_h_f_emp_idxs    = np.array([1, 0])
+    abs_h_f_emp_dB_idxs = np.array([1, 1])
 
     idxs_matrix = \
         np.vstack((abs_h_f_idxs,
-                   abs_h_f_dB_idxs))
+                   abs_h_f_dB_idxs,
+                   abs_h_f_emp_idxs,
+                   abs_h_f_emp_dB_idxs))
     
     num_rows = np.amax(idxs_matrix[:, 0]) + 1
     num_cols = np.amax(idxs_matrix[:, 1]) + 1
     [fig, axs] = plt.subplots(num_rows, num_cols)
 
     if(len(axs.shape) == 2):
-        abs_h_f_axis    = axs[   abs_h_f_idxs[0],    abs_h_f_idxs[1]]
-        abs_h_f_dB_axis = axs[abs_h_f_dB_idxs[0], abs_h_f_dB_idxs[1]]
+        abs_h_f_axis        = axs[       abs_h_f_idxs[0],        abs_h_f_idxs[1]]
+        abs_h_f_dB_axis     = axs[    abs_h_f_dB_idxs[0],     abs_h_f_dB_idxs[1]]
+        abs_h_f_emp_axis    = axs[   abs_h_f_emp_idxs[0],    abs_h_f_emp_idxs[1]]
+        abs_h_f_emp_dB_axis = axs[abs_h_f_emp_dB_idxs[0], abs_h_f_emp_dB_idxs[1]]
     else:
-        abs_h_f_axis    = axs[np.amax(   abs_h_f_idxs)]
-        abs_h_f_dB_axis = axs[np.amax(abs_h_f_dB_idxs)]
+        abs_h_f_axis        = axs[np.amax(       abs_h_f_idxs)]
+        abs_h_f_dB_axis     = axs[np.amax(    abs_h_f_dB_idxs)]
+        abs_h_f_emp_axis    = axs[np.amax(   abs_h_f_emp_idxs)]
+        abs_h_f_emp_dB_axis = axs[np.amax(abs_h_f_emp_dB_idxs)]
     
     abs_h_f_axis.plot(f, abs_h_f)
     abs_h_f_axis.set_xlim([0, 0.5])
@@ -64,6 +73,27 @@ def showPlots(f_c, f_c_1, f_c_2, f, abs_h_f, abs_h_f_dB):
     abs_h_f_dB_axis.set_ylabel(r'$|h_(f)|_{dB}$')
     abs_h_f_dB_axis.set_title('Target Frequency Magnitude Decibel Response')
 
+    abs_h_f_emp_axis.plot(f, abs_h_f_emp)
+    abs_h_f_emp_axis.set_xlim([0, 0.5])
+    #abs_h_f_emp_axis.set_ylim([-0.1, 1.1])
+    abs_h_f_emp_axis.axvline(f_c_1, color='r')
+    abs_h_f_emp_axis.axvline(f_c, color='k')
+    abs_h_f_emp_axis.axvline(f_c_2, color='r')
+    abs_h_f_emp_axis.grid()
+    abs_h_f_emp_axis.set_xlabel('Frequency (Hz)')
+    abs_h_f_emp_axis.set_ylabel(r'$|h_(f)|$')
+    abs_h_f_emp_axis.set_title('Acquired Frequency Magnitude Response')
+
+    abs_h_f_emp_dB_axis.plot(f, abs_h_f_emp_dB)
+    abs_h_f_emp_dB_axis.set_xlim([0, 0.5])
+    abs_h_f_emp_dB_axis.axvline(f_c_1, color='r')
+    abs_h_f_emp_dB_axis.axvline(f_c, color='k')
+    abs_h_f_emp_dB_axis.axvline(f_c_2, color='r')
+    abs_h_f_emp_dB_axis.grid()
+    abs_h_f_emp_dB_axis.set_xlabel('Frequency (Hz)')
+    abs_h_f_emp_dB_axis.set_ylabel(r'$|h_(f)|_{dB}$')
+    abs_h_f_emp_dB_axis.set_title('Acquired Frequency Magnitude Decibel Response')
+
     fig.tight_layout()
 
     plt.show()
@@ -77,6 +107,32 @@ if(__name__=='__main__'):
     transition_bandwidth_stop_perc = 0.6
     abs_h_f_c_stop_dB = -25
 
-    [f_c_1, f_c_2, f_c, f, abs_h_f, abs_h_f_dB] = calculate_frequency_magnitude_response(f_c, delta_f, transition_bandwidth, transition_bandwidth_stop_perc, abs_h_f_c_stop_dB)
+    [f_c_1, f_c_2, f_c, f, abs_h_f, abs_h_f_dB] = \
+        calculate_frequency_magnitude_response(f_c, delta_f, transition_bandwidth, transition_bandwidth_stop_perc, abs_h_f_c_stop_dB)
 
-    showPlots(f_c, f_c_1, f_c_2, f, abs_h_f, abs_h_f_dB)
+    reduction_factor = np.ceil(np.amax(np.abs(abs_h_f_dB))/10)
+    reduced_abs_h_f = np.power(abs_h_f, 1/reduction_factor)
+    squared_reduced_abs_h_f = np.square(reduced_abs_h_f)
+    squared_reduced_abs_h_f_cheb_poly_roots = utility.chebyshevSpectrumCalculations.calculateChebyshevSpectrumPolynomialRoots(f, delta_f, squared_reduced_abs_h_f, 40)
+    squared_reduced_abs_h_f_cheb_poly_root_dicts_list = utility.magnitudeDomainRoots.convertLimitedRootsArrayToRootsDictList(False, 'MA', squared_reduced_abs_h_f_cheb_poly_roots)
+
+    MA_z_coefs = np.array([1])
+    AR_z_coefs = np.array([1])
+    for root_dict in squared_reduced_abs_h_f_cheb_poly_root_dicts_list:
+        [tmp_MA_z_coefs, tmp_AR_z_coefs] = utility.frequencyResponseAndZTransformCalculations.z_trans_coefs(root_dict)
+        MA_z_coefs = np.polynomial.polynomial.polymul(MA_z_coefs, tmp_MA_z_coefs)
+        AR_z_coefs = np.polynomial.polynomial.polymul(AR_z_coefs, tmp_AR_z_coefs)
+
+    [_, h_f_emp] = dsp.freqz(MA_z_coefs, AR_z_coefs, 2*np.pi*f)
+    abs_h_f_emp = np.abs(h_f_emp)
+
+    #print(np.amax(abs_h_f_emp))
+    #MA_z_coefs = MA_z_coefs/np.amax(abs_h_f_emp)
+    #AR_z_coefs = AR_z_coefs/np.amax(abs_h_f_emp)
+
+    [_, h_f_emp] = dsp.freqz(MA_z_coefs/np.square(np.amax(abs_h_f_emp)), AR_z_coefs/np.square(np.amax(abs_h_f_emp)), 2*np.pi*f)
+    abs_h_f_emp = np.abs(h_f_emp)
+    abs_h_f_emp = abs_h_f_emp/np.amax(abs_h_f_emp)
+    abs_h_f_emp_dB = 10*np.log10(abs_h_f_emp)
+
+    showPlots(f_c, f_c_1, f_c_2, f, abs_h_f, abs_h_f_dB, abs_h_f_emp, abs_h_f_emp_dB)
